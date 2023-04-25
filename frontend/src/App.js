5766e39d14fdb16ff20983a4f7361b46b911bcdc
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-04-25 07:39:31 +0500

    v-03-create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div className="">
+      <header className="">
+        <a href="/">amazona</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;

55b5aef8f999c34e5cdb935bb01272b723f29bba
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-04-25 12:14:15 +0500

    Add products list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import data from "./data";
 
 
 function App() {
@@ -7,7 +8,25 @@ function App() {
         <a href="/">amazona</a>
       </header>
       <main>
-        list products
+        <h1>Featured Products</h1>
+        <div className='products'>
+          {
+            data.products.map(product => (
+              <div key={product.slug} className='product'>
+                <a href={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.name} />
+                </a>
+                <div className='product-info'>
+                  <a href={`/product/${product.slug}`}>
+                    <p>{product.name}</p>
+                  </a>
+                  <p><strong>${product.price}</strong></p>
+                  <button>Add to Cart</button>
+                </div>
+              </div>
+            ))
+          }
+        </div>
       </main>
     </div>
   );

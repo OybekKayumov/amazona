4d1d7be3823f384e3f4f8ba36d473a3924479ce4
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-04-26 07:29:53 +0500

    Add footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ import {LinkContainer} from "react-router-bootstrap";
 function App() {
   return (
     <BrowserRouter>
-      <div className="">
+      <div className="d-flex flex-column site-container">
         <header className="">
           <Navbar bg="dark" variant="dark">
             <Container>
@@ -22,12 +22,18 @@ function App() {
           </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path='/product/:slug' element={<ProductScreen />} />
-            <Route path='/' element={<HomeScreen />} />
-          </Routes>
-          
+          <Container>
+            <Routes>
+              <Route path='/product/:slug' element={<ProductScreen />} />
+              <Route path='/' element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className='text-center'>
+            ©2023 All rights reserved
+          </div>
+        </footer>
       </div>
     </BrowserRouter>
   );

51b717481997d5f037059ed8b5f5fe36ad89195c
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-04-28 07:46:04 +0500

    Add cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,7 +28,7 @@ function App() {
                   {
                     cart.cartItems.length > 0 && (
                       <Badge pill bg="danger">
-                        {cart.cartItems.length}
+                        {cart.cartItems.reduce((acc, curr) => acc + curr.quantity, 0)}
                       </Badge>
                     )
                   }
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -60,10 +60,10 @@ const ProductScreen = () => {
       window.alert('Sorry, Product is out of stock');
       return;
     }
-    
+
     ctxDispatch({
       type: 'CART_ADD_ITEM',
-      payload: {...product, quantity: 1},
+      payload: {...product, quantity},
     })
   }
 

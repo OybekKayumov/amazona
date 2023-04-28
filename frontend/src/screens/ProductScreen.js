ebc66c4d8ad16af9cc0fe1c0fbbff4b91202b2bc
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-04-28 06:21:54 +0500

    Add react context

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useReducer } from 'react';
+import React, { useContext, useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -12,6 +12,7 @@ import { Helmet } from 'react-helmet-async';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import { getError } from '../utils';
+import { Store } from '../store';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -48,6 +49,14 @@ const ProductScreen = () => {
     };
     fetchData();
   }, [slug])
+
+  const {state, dispatch: ctxDispatch} = useContext(Store);  
+  const addToCartHandler = () => {
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: {...product, quantity: 1},
+    })
+  }
 
   return (
     loading ? ( 
@@ -120,7 +129,10 @@ const ProductScreen = () => {
                   product.countInStock > 0 && (
                     <ListGroup.Item>
                       <div className='d-grid'>
-                        <Button variant='primary'>
+                        <Button 
+                          onClick={addToCartHandler}
+                          variant='primary'
+                        >
                           Add to Cart
                         </Button>
                       </div>

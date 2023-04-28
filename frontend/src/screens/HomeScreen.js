d31357aff16b9b8ca94d8c48e9fb9c36f55e274e
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-04-28 05:49:49 +0500

    Add bootstrap alert

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,6 +7,7 @@ import Col from 'react-bootstrap/Col';
 import Product from '../components/Product';
 import { Helmet } from 'react-helmet-async';
 import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 // import data from '../data';
 
 const reducer = (state, action) => {
@@ -57,7 +58,7 @@ function HomeScreen() {
                 // <div>Loading...</div>
                 <LoadingBox />
               ) : error ? ( 
-                <div>{error}</div> 
+                <MessageBox variant="danger">{error}</MessageBox> 
               ) : (
                 <Row>
                   {  products.map(product => (

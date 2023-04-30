d1f94b6a5be069ceb534a2da6b89f8ce65cb180d
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-04-30 13:36:14 +0500

    Add localStorege

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,9 +4,11 @@ export const Store = createContext();
 
 const initialState = {
   cart: {
-    cartItems: [],
-  }
-}
+    cartItems: localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems'))
+      : [],
+  },
+};
 
 function reducer(state, action) {
   switch (action.type) {
@@ -22,14 +24,14 @@ function reducer(state, action) {
         )
         : [...state.cart.cartItems, newItem];
 
-        localStorage.setItem('catItems', JSON.stringify(cartItems));
+        localStorage.setItem('cartItems', JSON.stringify(cartItems));
         return {...state, cart: { ...state.cart, cartItems } }
     case 'CART_REMOVE_ITEM': {
       const cartItems = state.cart.cartItems.filter(
         (item) => item._id !== action.payload._id
       );
-      
-        localStorage.setItem('catItems', JSON.stringify(cartItems));
+
+        localStorage.setItem('cartItems', JSON.stringify(cartItems));
         return {...state, cart: { ...state.cart, cartItems } }
       }
 

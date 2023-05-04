5f6bcec3720fe43fc59fd80dcddeb3ba345bc9e2
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-04 10:34:41 +0500

    Add shipping form

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -5,9 +5,12 @@ import Row from 'react-bootstrap/esm/Row'
 const CheckoutSteps = (props) => {
   return (
     <Row className='checkout-steps'>
-      <Col className={props.step1 ? 'active' : ''} ></Col>
+      <Col className={props.step1 ? 'active' : ''}>Sign-In</Col>
+      <Col className={props.step2 ? 'active' : ''}>Shipping</Col>
+      <Col className={props.step3 ? 'active' : ''}>Payment</Col>
+      <Col className={props.step4 ? 'active' : ''}>Place Order</Col>
     </Row>
   )
 }
 
-export default CheckoutSteps
+export default CheckoutSteps;

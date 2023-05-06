84588653aea62de1d13c32f73fe2bbd6bbb78da3
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-06 06:27:04 +0500

    Add payment method screen

--- frontend/src/screens/PaymentMethodScreen.js
+++ frontend/src/screens/PaymentMethodScreen.js
@@ -1,8 +1,15 @@
-import React from 'react'
-import CheckoutSteps from '../components/CheckoutSteps'
-import { Helmet } from 'react-helmet-async'
+import React from 'react';
+import CheckoutSteps from '../components/CheckoutSteps';
+import { Helmet } from 'react-helmet-async';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const PaymentMethodScreen = () => {
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+  }
+
   return (
     <div>
        <CheckoutSteps step1 step2 step3></CheckoutSteps>
@@ -12,6 +19,34 @@ const PaymentMethodScreen = () => {
           <title>Payment Method</title>
         </Helmet>
         <h1 className='my-3'>Payment Method</h1>
+
+        <Form onSubmit={submitHandler}>
+          <div className="mb-3">
+            <Form.Check
+              type='radio'
+              id='Paypal'
+              label='Paypal'
+              value='Paypal'
+              checked={paymentMethodName === 'Paypal'}
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-3">
+            <Form.Check
+              type='radio'
+              id='Stripe'
+              label='Stripe'
+              value='Stripe'
+              checked={paymentMethodName === 'Stripe'}
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-3">
+            <Button type='submit'>Continue</Button>
+          </div>
+        </Form>
        </div>
     </div>
   )

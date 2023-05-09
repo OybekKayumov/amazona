ee82b09db5fda125d2332b497822867365bbaf63
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-09 06:08:20 +0500

    Add order screen

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,9 +2,7 @@ import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js';
 import { isAuth } from '../utils.js';
-
 const orderRouter = express.Router();
-
 orderRouter.post(
   '/',
   isAuth,
@@ -19,9 +17,21 @@ orderRouter.post(
       totalPrice: req.body.totalPrice,
       user: req.user._id,
     });
-
     const order = await newOrder.save();
     res.status(201).send({ message: 'New Order Created', order });
+  })
+);
+
+orderRouter.get(
+  '/:id',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      res.send(order);
+    } else {
+      res.status(404).send({ message: 'Order Not Found' });
+    }
   })
 );
 
